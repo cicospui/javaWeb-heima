bc88fade2987a85d63f27557f50e6b47c895b952
cicospui <1249828424@qq.com>
2023-11-23 18:46:04 +0800

    day04

---
+++ code/day04-maven和web入门/maven-project01/src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import java.util.logging.Logger;
+
+/**
+ * @Description:
+ * @Author: 徐晓宇
+ * @Date: 2023/11/23 14:20
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger logger;
+        System.out.println("HelloWorld");
+    }
+}
