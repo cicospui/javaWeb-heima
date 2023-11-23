b2b86f9f11793187d9e6de32c983623bd2f43b8a
cicospui <1249828424@qq.com>
2023-11-23 13:42:33 +0800

    day02~03

---
+++ code/day03-ajax-VUE-ELEMENT/VUE/vur-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <element-view></element-View> -->
+    <emp-view>
+
+    </emp-view>
+  </div>
+</template>
+
+<script>
+// import ElementView from './views/Element/ElementView.vue'
+import EmpView from './views/tlias/EmpView.vue'
+export default{
+  // components:{ ElementView },
+  components:{EmpView},
+
+  data(){
+    return {
+      message: "hello Vue"
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ code/day03-ajax-VUE-ELEMENT/VUE/vur-project/src/views/tlias/EmpView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <el-container style="height: 500px; border: 1px solid #eee">
+            <el-header style="font-size: 40px;background-color: rgb(238,241,246)">tlias智能学习辅助系统</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <el-menu :default-openeds="['1', '3']">
+                        <el-submenu index="1">
+                            <template slot="title"><i class="el-icon-message"></i>信息管理系统</template>
+                            
+                            <el-menu-item index="1-1">部门管理</el-menu-item>
+                            <el-menu-item index="1-2">员工管理</el-menu-item>
+                                                   
+                        </el-submenu>
+                    </el-menu>
+                </el-aside>
+                <el-main>
+                    <el-table :data="tableData">
+                        <el-table-column prop="date" label="日期" width="140">
+                        </el-table-column>
+                        <el-table-column prop="name" label="姓名" width="120">
+                        </el-table-column>
+                        <el-table-column prop="address" label="地址">
+                        </el-table-column>
+                    </el-table>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default{
+
+
+}
+
+</script>
+
+<style>
+
+
+</style>
+
+
